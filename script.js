a5198b355e45da0b498e6ba14e26c7a804925fba
DenysL <liashenko.denys@gmail.com>
2023-10-06 13:51:20 +0200

    dodany bohater

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const CANVAS_HEIGHT = 1000;
+const CANVAS_WIDTH = 1000;
+
 const PLATFORM_HEIGHT = 200
 const PLATFORM_POSITION_HEIGHT = 400
 const PLATFORM_MIN_GAP = 40;
@@ -10,7 +13,13 @@ const PLATFORM_MAX_WIDTH = 100;
 const PLATFORM_POSITION_START = 50
 const PLATFORM_WIDTH_START = 50
 
+const HERO_WIDTH = 17; 
+const HERO_HEIGHT = 25;
+
 platforms = [{ xPosition: PLATFORM_POSITION_START, width: PLATFORM_WIDTH_START }];
+
+studentX = platforms[0].xPosition + platforms[0].width / 2
+studentY = 0;
 
 function generatePlatform() {
   const lastPlatform = platforms[platforms.length - 1];
@@ -30,8 +39,52 @@ function generatePlatforms(number) {
     ctx.fillRect(platforms[i].xPosition, PLATFORM_POSITION_HEIGHT, platforms[i].width, PLATFORM_HEIGHT);
 }
 
-generatePlatforms(5)
+function drawStudent() {
+ 
+  ctx.save();
+  ctx.fillStyle = "black";
+  ctx.translate(studentX,PLATFORM_POSITION_HEIGHT - 28 );
 
+  // Body
+  ctx.fillStyle = "black";
+  ctx.roundRect(0, 0, HERO_WIDTH, HERO_HEIGHT, 5);
+  ctx.fill();
+
+  // Legs
+  const LEG_DISTANCE = 3;
+  ctx.beginPath();
+  ctx.arc(LEG_DISTANCE, HERO_HEIGHT, 3, 0, Math.PI * 2, false);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(HERO_WIDTH - LEG_DISTANCE, HERO_HEIGHT, 3, 0, Math.PI * 2, false);
+  ctx.fill();
+
+  // Eye
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(HERO_WIDTH - LEG_DISTANCE, HERO_HEIGHT-(HERO_HEIGHT-5), 3, 0, Math.PI * 2, false);
+  ctx.fill();
+
+  // Band
+  ctx.fillStyle = "blue";
+  ctx.fillRect(-1, 2, HERO_WIDTH, 4);
+  ctx.beginPath();
+  ctx.moveTo(-3, 0);
+  ctx.lineTo(-9, -6);
+  ctx.lineTo(-6, 5);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(-1, 2);
+  ctx.lineTo(-8, 10);
+  ctx.lineTo(2, 6);
+  ctx.fill();
+
+  ctx.restore();
+  
+}
+
+generatePlatforms(5)
+drawStudent()
 // ctx.beginPath();
 // ctx.lineWidth = 2;
 // ctx.moveTo(PLATFORM_POSITION_START + PLATFORM_WIDTH_START - 1, PLATFORM_POSITION_HEIGHT);
