d2e13fcd8fba4fc6bb92d2fa85da173b0a847cb6
gajewskimjanasjliashenkod <gajewskimjanasjliashenkod@gmail.com>
2023-11-14 20:52:18 +0100

    refaktoryzacja

--- script.js
+++ script.js
@@ -1,331 +1,270 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+let status = 'waiting';
+let lastTimestamp;
+let studentPositionX;
+let studentPositionY;
+let sceneTranslation;
+let platforms = [];
+let sticks = [];
+let points = 0;
 
-const PLATFORM_HEIGHT = 200
-const PLATFORM_POSITION_HEIGHT = 400
-const PLATFORM_MIN_GAP = 40;
-const PLATFORM_MAX_GAP = 200;
-const PLATFORM_MIN_WIDTH = 20;
-const PLATFORM_MAX_WIDTH = 100;
-const PLATFORM_POSITION_START = 50
-const PLATFORM_WIDTH_START = 50
-const NUMBER_OF_PLATFORMS = 5
+const STUDENT_WIDTH = 17;
+const STUDENT_HEIGHT = 30;
 
-const HERO_WIDTH = 17; 
-const HERO_HEIGHT = 25;
+const CANVAS_WIDTH = 375;
+const CANVAS_HEIGHT = 375;
+const PLATFORM_HEIGHT = 100;
+const PLATFORM_BONUS_SIZE = 10;
+const canvas = document.getElementById('game');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
 
-const STICK_WIDTH = 3;
+const instructionEl = document.getElementById('instruction');
+const bonusEl = document.getElementById('bonus');
+const restartButton = document.getElementById('restart');
+const pointsEl = document.getElementById('points');
 
-platforms = [{ xPosition: PLATFORM_POSITION_START, width: PLATFORM_WIDTH_START }];  // [początek platformy w osi X, szerokość platformy]
-
-const x = PLATFORM_POSITION_START + (PLATFORM_WIDTH_START / 2);
-const y = x  - PLATFORM_POSITION_START;
-const PLATFORM_POSITION_START_SMAL = PLATFORM_POSITION_START + y;
-const PLATFORM_WIDTH_START_Smal = PLATFORM_WIDTH_START / 2;
-const PLATFORM_HEIGHT_Point_end = 400;
-const PLATFORM_HEIGHT_Point_Start = 395
-platformsPoint = [{ xPosition: PLATFORM_POSITION_START_SMAL, width: PLATFORM_WIDTH_START_Smal}];
-
-
-studentX = platforms[0].xPosition + platforms[0].width / 2
-studentY = 0;
-currentPlatformIndex = 0;  // indeks platformy, na której aktualnie będzie rysowana belka
-
-pointStudnet = 0;
-indexStudentInPlatform = 0;
-
-let stickStartX = PLATFORM_POSITION_START + PLATFORM_WIDTH_START - 1
-let stickStartY = PLATFORM_POSITION_HEIGHT
-let stickEndX
-let stickEndY
-let isDrawingRising = false;
-let isDrawingRotate = false;
-let isDrawingFalling = false;
-let stickLength = 0;
-let stickRotationAngle = 0;
-let xRange = 0;
-let intervalID;
-
-sticks = [{ xPosition: PLATFORM_POSITION_START + PLATFORM_WIDTH_START, width: stickLength}]
+Array.prototype.last = function() {
+    return this[this.length - 1];
+};
 
 
+resetGame();
 
-function generatePlatformPoint() 
-{
-  console.log( platforms.length);
-  for(let i = 1; i < platforms.length; ++i)
-  {
-    let platform = platforms[i];
-    
-    
-    let polowa = platform.xPosition + (platform.width / 2);
-    let polowa2 = polowa  - platform.xPosition;
-    const platformPossion = platform.xPosition + polowa2;
-    const platformWidt = platform.width / 2;
-    platformsPoint.push({ platformPossion, platformWidt });
-    console.log(platformsPoint);
-  }
-}
 
-function generatePlatformsPoint() 
-{
+function resetGame() {
+    status = 'waiting';
+    lastTimestamp = undefined;
+    sceneTranslation = 0;
+    points = 0;
+    pointsEl.innerText = points;
+    platforms = [{x: 50, w: 50}];
+    for (let i = 0; i < 4; ++i)
+        generatePlatform();
 
-  generatePlatformPoint();
-  for (let i = 0; i < platformsPoint.length; ++i)
-    ctx.fillRect(platformsPoint[i].xPosition, 380, platformsPoint[i].width, 10);
-}
+    sticks = [{x: platforms[0].x + platforms[0].w, length: 0, rotation: 0}];
 
-function regeneratePlatformsPoint() 
-{
-  ctx.beginPath();
-  for (let i = 0; i < platformsPoint.length; ++i)
-    ctx.fillRect(platformsPoint[i].xPosition, 380, platformsPoint[i].width, 10);
-    ctx.stroke();
-}
-
-function checkUserAlive()
-{
-	 if(studentX + stickLength >= platforms[indexStudentInPlatform + 1].xPosition && studentX + stickLength <= platforms[indexStudentInPlatform + 1].xPosition + platforms[indexStudentInPlatform + 1].width)
-	 {
-		 indexStudentInPlatform++;
-		 pointStudnet += 10;
-		 return true;
-	 } 
-	 return false;
-}
-
-function checkUserAlive1()
-{
-	for(let i = indexStudentInPlatform; i < platforms.length() - 1; ++i)
-	{
-	 if(studentX + stickLength >= platforms[i + 1].xPosition && studentX + stickLength <= platforms[i + 1].xPosition + platforms[i + 1].width)
-	 {
-		 indexStudentInPlatform += i;
-		 pointStudnet += 10 * i;
-		 return true;
-	 }
-	}
-	return false;
-}
-
-function draw() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.lineWidth = STICK_WIDTH;
-  regeneratePlatforms();
-  regeneratePlatformsPoint();
-  drawStudent(xRange);
-  drawSticks()
-}
-
-function drawSticks() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < sticks.length; ++i) {
-    ctx.beginPath();
-    ctx.moveTo(sticks[i].xPosition, stickStartY);
-    ctx.lineTo(sticks[i].xPosition + sticks[i].width, stickStartY);
-    ctx.stroke();
-  }
+    studentPositionX = platforms[0].x + platforms[0].w - 10;
+    studentPositionY = 0;
+    draw()
 }
 
 function generatePlatform() {
-  const lastPlatform = platforms[platforms.length - 1];
-  let furthestX = lastPlatform.xPosition + lastPlatform.width;
+    const platformMinGap = 40;
+    const platformMaxGap = 200;
+    const minWidth = 20;
+    const maxWidth = 100;
 
-  const xPosition = furthestX + PLATFORM_MIN_GAP + Math.floor(Math.random() * (PLATFORM_MAX_GAP - PLATFORM_MIN_GAP));
-  const width = PLATFORM_MIN_WIDTH + Math.floor(Math.random() * (PLATFORM_MAX_WIDTH - PLATFORM_MIN_WIDTH));
+    const lastPlatform = platforms[platforms.length - 1];
+    let furthestX = lastPlatform.x + lastPlatform.w;
 
-  platforms.push({ xPosition, width });
+    const x = furthestX + platformMinGap + Math.floor(Math.random() * (platformMaxGap - platformMinGap));
+    const w = minWidth + Math.floor(Math.random() * (maxWidth - minWidth));
+
+    platforms.push({x, w});
 }
 
-function generatePlatforms(number) {
-  for (let i = 0; i < number - 1; ++i)
-    generatePlatform()
-
-  for (let i = 0; i < platforms.length; ++i)
-    ctx.fillRect(platforms[i].xPosition, PLATFORM_POSITION_HEIGHT, platforms[i].width, PLATFORM_HEIGHT);
-}
-
-function regeneratePlatforms() {
-  for (let i = 0; i < platforms.length; ++i)
-    ctx.fillRect(platforms[i].xPosition, PLATFORM_POSITION_HEIGHT, platforms[i].width, PLATFORM_HEIGHT);
-}
-
-function drawStudent(xRange) {
-
-  ctx.save();
-  ctx.fillStyle = "black";
-  ctx.translate(studentX + xRange,PLATFORM_POSITION_HEIGHT - 28 );
-
-  // Body
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.roundRect(0, 0, HERO_WIDTH, HERO_HEIGHT, 5);
-  ctx.fill();
-  ctx.stroke();
-
-  // Legs
-  const LEG_DISTANCE = 3;
-  ctx.beginPath();
-  ctx.arc(LEG_DISTANCE, HERO_HEIGHT, 3, 0, Math.PI * 2, false);
-  ctx.fill();
-  ctx.beginPath();
-  ctx.arc(HERO_WIDTH - LEG_DISTANCE, HERO_HEIGHT, 3, 0, Math.PI * 2, false);
-  ctx.fill();
-  ctx.stroke();
-
-  // Eye
-  ctx.beginPath();
-  ctx.fillStyle = "white";
-  ctx.arc(HERO_WIDTH - LEG_DISTANCE, HERO_HEIGHT-(HERO_HEIGHT-5), 3, 0, Math.PI * 2, false);
-  ctx.fill();
-  ctx.stroke();
-
-  // Band
-  ctx.fillStyle = "blue";
-  ctx.fillRect(-1, 2, HERO_WIDTH, 4);
-  ctx.beginPath();
-  ctx.moveTo(-3, 0);
-  ctx.lineTo(-9, -6);
-  ctx.lineTo(-6, 5);
-  ctx.fill();
-  ctx.beginPath();
-  ctx.moveTo(-1, 2);
-  ctx.lineTo(-8, 10);
-  ctx.lineTo(2, 6);
-  ctx.fill();
-  ctx.stroke();
-
-  ctx.restore();
-  
-}
-
-function ifStickTouchPlatform() {
-  if (currentPlatformIndex < NUMBER_OF_PLATFORMS)
-    if (platforms[currentPlatformIndex].xPosition <= stickEndX &&
-    platforms[currentPlatformIndex].xPosition + platforms[currentPlatformIndex].width >= stickEndX &&
-    stickEndY.toFixed(2) == PLATFORM_POSITION_HEIGHT)  // stickEndY ostatecznie wynosi np. 400.00000000000017, więc toFixed()
-      return true
-      
-  return false
-}
-
-function drawLine(before = false) { // before oznacza, że odnosimy się do poprzedniej platformy
-  if (before && currentPlatformIndex > 0)
-    stickStartX = platforms[currentPlatformIndex - 1].xPosition + platforms[currentPlatformIndex - 1].width - 1
-  else
-    stickStartX = platforms[currentPlatformIndex].xPosition + platforms[currentPlatformIndex].width - 1
-  
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.moveTo(stickStartX, stickStartY);
-  ctx.lineTo(stickEndX, stickEndY);
-  ctx.stroke();
-}
-
-function animateStudent() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // drawLine();
-  drawSticks()
-  draw();
-  drawStudent(xRange);
-  xRange++; 
-  if (studentX + xRange + HERO_WIDTH >= stickEndX){
-    clearInterval(intervalID)
-  }
-}
-function animateStickRising() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  stickLength += 1;
-
-  stickEndX = stickStartX;
-  stickEndY = stickStartY - stickLength;
-
-  drawLine();
-  draw();
-
-  if (isDrawingRising)
-    requestAnimationFrame(animateStickRising);
-}
-
-function animateStickRotating() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  stickRotationAngle += Math.PI / 180 * 2;  // obrot o 2 stopnie
-  
-  stickEndX = stickStartX + Math.sin(stickRotationAngle) * stickLength;
-  stickEndY = stickStartY + (-Math.cos(stickRotationAngle)) * stickLength;
-
-  drawLine();
-  draw();
-  
-  if (stickRotationAngle > Math.PI / 2) // stickRotationAngle > 90 stopni
-    isDrawingRotate = false;
-
-  if (isDrawingRotate)
-    requestAnimationFrame(animateStickRotating);
-  else {
-    isDrawingFalling = true
-    requestAnimationFrame(animateStickFalling)
-    if (currentPlatformIndex < NUMBER_OF_PLATFORMS - 1 && stickEndY.toFixed(2) == PLATFORM_POSITION_HEIGHT) {
-      // isDrawingFalling = true;
-      currentPlatformIndex++
+window.addEventListener('mousedown', () => {
+    if (status == "waiting") {
+        lastTimestamp = undefined;
+        instructionEl.style.opacity = 0;
+        status = 'stretching';
+        window.requestAnimationFrame(animate);
     }
-  }
-
-  if (ifStickTouchPlatform()) {
-    len = platforms[currentPlatformIndex - 1].xPosition + platforms[currentPlatformIndex - 1].width - 1
-    sticks.push({ xPosition: len, width: stickEndX - stickStartX });
-    isDrawingFalling = false;
-    drawSticks()
-  }
-}
-
-function animateStickFalling() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  stickRotationAngle += Math.PI / 180 * 2;  // obrot o 2 stopnie
-  
-  stickEndX = stickStartX + Math.sin(stickRotationAngle) * stickLength;
-  stickEndY = stickStartY + (-Math.cos(stickRotationAngle)) * stickLength;
-
-  if (currentPlatformIndex < NUMBER_OF_PLATFORMS - 1)  // belka rysowana na poprzedniej platformie, gdyż currentPlatformIndex została zwiększona o 1
-    drawLine(true);  // currentPlatformIndex została zwiększona o 1, więc przekazujemy wartość true, by się odnieść do poprzedniej platformy
-  else
-    drawLine()  // belka rysowana na ostatniej platformie się nie cofa 
-
-  draw();
-
-  if (stickRotationAngle > Math.PI)  // stickRotationAngle > 180 stopni
-    isDrawingFalling = false;
-  
-  if (isDrawingFalling)
-    requestAnimationFrame(animateStickFalling);
-  else
-    stickLength = 0
-}
-
-
-canvas.addEventListener('mousedown', () => {
-  isDrawingRising = true;
-  animateStickRising();
 });
 
-canvas.addEventListener('mouseup', () => {
-  stickRotationAngle = 0;
-  isDrawingRising = false;
-  isDrawingRotate = true;
-  animateStickRotating();
-  intervalID = setInterval(animateStudent, 10);
-});
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.addEventListener('mouseup', () => {
+    if (status == "stretching") {
+        status = "turning";
+    }
+});
 
 window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  draw();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
 });
 
+function animate(timestamp) {
+    if (!lastTimestamp) {
+        lastTimestamp = timestamp;
+        window.requestAnimationFrame(animate);
+        return;
+    }
 
-generatePlatforms(NUMBER_OF_PLATFORMS);
-generatePlatformPoint();
-draw();
+    switch (status) {
+        case "waiting":
+            return;
+        case "stretching":{
+            sticks.last().length += (timestamp - lastTimestamp) / 4;
+            break;
+        }
+
+        case 'turning': {
+            sticks.last().rotation += (timestamp - lastTimestamp) / 4;
+
+            if (sticks.last().rotation > 90) {
+                sticks.last().rotation = 90;
+
+                const [nextPlatform, bonusHit] = platformHit();
+                if (nextPlatform) {
+                    points += 1
+                    pointsEl.innerText = points;
+                    generatePlatform();
+                }
+                status = 'walking';
+            }
+            break;
+        }
+        case 'walking': {
+            studentPositionX += (timestamp - lastTimestamp) / 4;
+            const [nextPlatform] = platformHit();
+            if(nextPlatform) {
+                const maxHeroX = nextPlatform.x + nextPlatform.w - 10;
+                if(studentPositionX > maxHeroX) {
+                    studentPositionX = maxHeroX;
+                    status = "transitioning";
+                }
+            } else {
+                const maxHeroX = sticks.last().x + sticks.last().length + STUDENT_WIDTH;
+                if (studentPositionX > maxHeroX) {
+                    studentPositionX = maxHeroX;
+                    status = 'falling';
+                }
+            }
+            break;
+        }
+        case "transitioning": {
+            sceneTranslation += (timestamp - lastTimestamp) / 3;
+            const [nextPlatform] = platformHit();
+            if (sceneTranslation > nextPlatform.x + nextPlatform.w - 100) {
+                sticks.push({
+                    x: nextPlatform.x + nextPlatform.w,
+                    length: 0,
+                    rotation: 0
+                });
+                status = "waiting";
+            }
+            break;
+        }
+        case "falling": {
+            if(sticks.last().rotation < 180)
+            sticks.last().rotation += (timestamp - lastTimestamp) / 4;
+            studentPositionY += (timestamp - lastTimestamp) / 2;
+            const maxStudentFallingY = PLATFORM_HEIGHT + 100 + (window.innerHeight - CANVAS_HEIGHT) / 2;
+            if(studentPositionY > maxStudentFallingY) {
+                restartButton.style.display = "block";
+                return;
+            }
+            break;
+        }
+    }
+
+    draw();
+    window.requestAnimationFrame(animate);
+    lastTimestamp = timestamp;
+}
+
+function platformHit() {
+    const stickFarX = sticks.last().x + sticks.last().length;
+    const platformHit = platforms.find((platform) => {
+        const platformFarX = platform.x + platform.w;
+        return platform.x < stickFarX && stickFarX < platformFarX;
+    });
+
+    if ( platformHit && isStickInBonusArea(platformHit, stickFarX) ) {
+        return [platformHit, true];
+    }
+    return [platformHit, false];
+}
+
+function isStickInBonusArea(platform, stickFarX) {
+    const bonusAreaStartX = platform.x + platform.w / 2 - PLATFORM_BONUS_SIZE / 2;
+    const bonusAreaEndX = platform.x + platform.w / 2 + PLATFORM_BONUS_SIZE / 2;
+    return bonusAreaStartX < stickFarX && stickFarX < bonusAreaEndX;
+}
+
+function draw() {
+    ctx.save();
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    drawPlatforms();
+    drawStudent();
+    drawSticks();
+    ctx.restore();
+}
+
+restartButton.addEventListener('click', () => {
+    resetGame();
+    restartButton.style.display = 'none';
+});
+
+function drawPlatforms() {
+    platforms.forEach(({x, w}) =>{
+        ctx.fillStyle = 'black';
+        ctx.fillRect(x, CANVAS_HEIGHT - PLATFORM_HEIGHT, w, PLATFORM_HEIGHT + (window.innerHeight - CANVAS_HEIGHT) / 2);
+    });
+}
+
+function drawStudent() {
+    ctx.save();
+    ctx.fillStyle = 'blue';
+    ctx.translate(studentPositionX - STUDENT_WIDTH / 2 , studentPositionY + CANVAS_HEIGHT - PLATFORM_HEIGHT - STUDENT_HEIGHT / 2);
+
+    drawBody(-STUDENT_WIDTH / 2, -STUDENT_HEIGHT / 2, STUDENT_WIDTH, STUDENT_HEIGHT - 4, 5);
+    const legDistance = 5;
+    ctx.beginPath();
+    ctx.arc(legDistance, 11.5, 3, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(-legDistance, 11.5, 3, 0, Math.PI * 2, false);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.arc(5, -7, 3, 0, Math.PI * 2 , false);
+    ctx.fill()
+    ctx.fillStyle = "red";
+    ctx.fillRect(-STUDENT_WIDTH / 2 - 1, -12, STUDENT_WIDTH + 2, 4.5);
+    ctx.beginPath();
+    ctx.moveTo(-9, -14.5);
+    ctx.lineTo(-17, -18.5);
+    ctx.lineTo(-14, -8.5);
+    ctx.fill()
+    ctx.beginPath();
+    ctx.moveTo(-10, -10.5);
+    ctx.lineTo(-15, -3.5);
+    ctx.lineTo(-5, -7);
+    ctx.fill();
+    ctx.restore();
+}
+
+function drawBody(x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.arcTo(x, y + height, x + radius, y + height, radius);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+    ctx.lineTo(x + width, y + radius)
+    ctx.arcTo(x + width, y, x + width - radius, y, radius);
+    ctx.lineTo(x + radius, y);
+    ctx.arcTo(x, y, x, y + radius, radius);
+    ctx.fill();
+}
+
+function drawSticks() {
+    sticks.forEach((stick) => {
+        ctx.save();
+        ctx.translate(stick.x, CANVAS_HEIGHT - PLATFORM_HEIGHT);
+        ctx.rotate((Math.PI / 180) * stick.rotation);
+
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, -stick.length);
+        ctx.stroke();
+        ctx.restore();
+    });
+}
